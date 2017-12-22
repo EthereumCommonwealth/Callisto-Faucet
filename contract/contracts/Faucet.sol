e20700c81075f5a12fb67d0f4073a9304e0149e1
Yohan Graterol <yohangraterol92@gmail.com>
2017-12-21 23:13:51 -0500

    :tada: First Faucet smart contract version

---
+++ contract/contracts/Faucet.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.4.18;
+
+import "./Ownable.sol";
+
+
+contract Faucet is Ownable {
+    address owner;
+    uint    blockLimit = 10;
+    uint    amountEther = 10 ether;
+    mapping (address => uint) lastFaucet;
+
+    // Constructor
+    function Faucet() public Ownable() {
+    }
+
+    function () public payable {}
+
+    // External functions
+    function sendEther(address account) public onlyOwner {
+        require(this != account);
+        require(account != address(0)); // Please, use a normal address and after funds your contract.
+        require(canReceiveEther(account) == true);
+
+        lastFaucet[account] = block.number;
+        account.transfer(amountEther);
+    }
+
+    function setBlockLimit(uint newBlockLimit) public onlyOwner {
+        require(newBlockLimit > 0);
+        blockLimit = newBlockLimit;
+    }
+
+    function setAmountEther(uint newAmountEther) public onlyOwner {
+        require(newAmountEther >= 1 ether);
+        amountEther = newAmountEther;
+    }
+
+    function checkRemainingBlock(address account) public view returns (uint) {
+        return lastFaucet[account] - (block.number - blockLimit);
+    }
+
+    // Internal and Private functions
+    function canReceiveEther(address account) private view returns (bool) {
+        var lastFaucetBlock = lastFaucet[account];
+
+        if (getBlockNumber() > lastFaucetBlock && checkContractBalance()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    function getBlockNumber() private view returns (uint) {
+        return block.number - blockLimit;
+    }
+
+    function checkContractBalance() private view returns (bool) {
+        return this.balance > amountEther;
+    }
+}
