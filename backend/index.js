d94807e08ffc90c0d98c6f6f595cd735e5700aa0
Yohan Graterol <yohangraterol92@gmail.com>
2017-12-30 21:52:54 -0500

    :tada: Faucet Backend

---
+++ backend/index.js
@@ -0,0 +1,78 @@
+import fs from "fs";
+import express from "express";
+import bodyParser from "body-parser";
+import Web3 from "web3";
+import Tx from "ethereumjs-tx";
+
+const app = express()
+app.use(bodyParser.json());
+
+// Environment vars
+const fromAddress = process.env.FROM_ADDRESS;
+const privateKey = new Buffer(process.env.PRIVATE_KEY, "hex");
+const contractAddress = process.env.CONTRACT_ADDRESS;
+const rpcServer = process.env.RPC_SERVER;
+
+// const web3 = new Web3("https://testnet.callisto.network/");
+const web3 = new Web3(rpcServer);
+
+const faucetABI = JSON.parse(fs.readFileSync("faucet-abi.json", "utf8"));
+
+const Faucet = new web3.eth.Contract(faucetABI, contractAddress);
+
+async function sendTransaction(address) {
+  let remainingBlocks = await Faucet.methods.checkRemainingBlocks(address).call({from: fromAddress});
+  remainingBlocks = parseInt(remainingBlocks);
+
+  if (remainingBlocks > 0) {
+    return [false, `Your address has been funded. Please wait for ${remainingBlocks} blocks.`];
+  }
+
+  let sendCLO = Faucet.methods.sendEther(address)
+
+  // Construct the raw transaction
+  const gasPriceHex = web3.utils.toHex(2000000000000);
+  const gasLimitHex = web3.utils.toHex(300000);
+
+  const nonce = await web3.eth.getTransactionCount(fromAddress);
+  const nonceHex = web3.utils.toHex(nonce);
+
+  const rawTx = {
+    nonce: nonceHex,
+    gasPrice: gasPriceHex,
+    gasLimit: gasLimitHex,
+    data: sendCLO.encodeABI(),
+    from: fromAddress,
+    to: contractAddress,
+    chainId: 7919
+  };
+
+  const tx = new Tx(rawTx);
+  tx.sign(privateKey);
+  const serializedTx = tx.serialize();
+
+  try{
+    const transaction = await web3.eth.sendSignedTransaction("0x" + serializedTx.toString("hex"));
+    return [transaction.transactionHash, false];
+  } catch (error) {
+    return [false, error];
+  }
+}
+
+app.post("/faucet", async (req, res) => {
+  const faucetResult = await sendTransaction(req.body.address);
+  const resBody = {
+    transactionHash: faucetResult[0] ? faucetResult[0] : null,
+    error: faucetResult[1] ? faucetResult[1] : null
+  }
+  const resCode = faucetResult[0] ? 200 : 409
+  res.status(resCode).send(resBody);
+})
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    "me": "I'm the Callisto Faucet System."
+  })
+})
+
+app.listen(8080, () => console.log("I'm the Callisto Faucet System on port 8080!"));
