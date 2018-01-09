95f4a7a3628fd7be7cb0889a2d3cbfbd3619ae7c
Carlos Sampol <sampol.90@gmail.com>
2018-01-08 21:53:24 -0500

    Fixing github styles

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -56,31 +56,33 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <h1 className="App-title" >Callisto Faucet</h1>
-        <p className="App-intro">This is the Callisto faucet system for testnet</p>
-        <form className="App-form" onSubmit={this.onSubmit}>
-          <input
-            className="App-form-input"
-            value={this.state.address}
-            onChange={this.onChange}
-            placeholder="Address..."
-            type="text"
-            required
-          />
-          <a className="App-form-submit" onClick={(evt) => {
-            if (this.state.address !== ""){
-              this.onSubmit(evt);
-            }            
-          }}>Submit</a>
-        </form>
-        {this.state.showSpinner ? <i className="fas fa-circle-notch fa-spin fa-3x fa-fw App-Spinner" /> : null}
-        {this.state.message ? <p className="App-message">transactionHash: {this.state.message}</p> : null}
-        {this.state.error ? <p className="App-error">{this.state.error}</p> : null}
-        <footer className="Footer">
+      <div className="App-Container">
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <h1 className="App-title" >Callisto Faucet</h1>
+          <p className="App-intro">This is the Callisto faucet system for testnet</p>
+          <form className="App-form" onSubmit={this.onSubmit}>
+            <input
+              className="App-form-input"
+              value={this.state.address}
+              onChange={this.onChange}
+              placeholder="Address..."
+              type="text"
+              required
+            />
+            <a className="App-form-submit" onClick={(evt) => {
+              if (this.state.address !== ""){
+                this.onSubmit(evt);
+              }            
+            }}>Submit</a>
+          </form>
+          {this.state.showSpinner ? <i className="fas fa-circle-notch fa-spin fa-3x fa-fw App-Spinner" /> : null}
+          {this.state.message ? <p className="App-message">transactionHash: {this.state.message}</p> : null}
+          {this.state.error ? <p className="App-error">{this.state.error}</p> : null}
+        </div>
+        <footer className="Footer" key="footer">
           <a
             href="https://github.com/EthereumCommonwealth/Callisto-Faucet"
             target="_blank"
